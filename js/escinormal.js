82149cd50ca0ec26ae831f8532579bf8c27d8259
Gordon Moore <gmgit@gordonmoore.co.uk>
2020-07-15 17:18:17 +0100

    Initial setup of code framework

---
+++ js/escinormal.js
@@ -0,0 +1,21 @@
+/*
+Program       esci.js
+Author        Gordon Moore
+Date          14 July 2020
+Description   The JavaScript code for esci-wb
+Licence       GNU General Public LIcence Version 3, 29 June 2007
+*/
+
+// #region Version history
+/*
+0.1.0                 Initial version
+*/
+//#endregion 
+
+let version = '0.0.1';
+
+'use strict';
+$(function() {
+  console.log('jQuery here!');  //just to make sure everything is working
+
+})
